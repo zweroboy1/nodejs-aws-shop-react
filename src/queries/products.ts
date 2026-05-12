5335305c4ec4982dbfc624740ea96faa8ee520bc
Oleksii Roman <fprognoz@gmail.com>
2026-05-12 17:43:09 +0300

    fix: update post link

--- src/queries/products.ts
+++ src/queries/products.ts
@@ -48,7 +48,7 @@ export function useRemoveProductCache() {
 
 export function useUpsertAvailableProduct() {
   return useMutation((values: AvailableProduct) =>
-    axios.put<AvailableProduct>(`${API_PATHS.bff}/product`, values, {
+    axios.post<AvailableProduct>(`${API_PATHS.bff}/products`, values, {
       headers: {
         Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
       },

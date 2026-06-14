b229c7ebc08800da9c3b9af6e7ab2930512f2c53
Oleksii Roman <fprognoz@gmail.com>
2026-06-14 06:08:37 +0300

    feat: update endpoints for orders and cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://39r1iqoj3f.execute-api.eu-central-1.amazonaws.com/prod",
-  bff: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+    product: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
+    order: "https://pxf5ekte6d.execute-api.eu-central-1.amazonaws.com/prod/api/profile/cart",
+    import: "https://39r1iqoj3f.execute-api.eu-central-1.amazonaws.com/prod",
+    bff: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
+    cart: "https://pxf5ekte6d.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;
--- src/queries/orders.ts
+++ src/queries/orders.ts
@@ -6,58 +6,62 @@ import { OrderStatus } from "~/constants/order";
 import { Order } from "~/models/Order";
 
 export function useOrders() {
-  return useQuery<Order[], AxiosError>("orders", async () => {
-    const res = await axios.get<Order[]>(`${API_PATHS.order}/order`);
-    return res.data;
-  });
+    return useQuery<Order[], AxiosError>("orders", async () => {
+        const res = await axios.get<Order[]>(`${API_PATHS.order}/order`, {
+            headers: {
+                Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+            },
+        });
+        return res.data;
+    });
 }
 
 export function useInvalidateOrders() {
-  const queryClient = useQueryClient();
-  return React.useCallback(
-    () => queryClient.invalidateQueries("orders", { exact: true }),
-    []
-  );
+    const queryClient = useQueryClient();
+    return React.useCallback(
+        () => queryClient.invalidateQueries("orders", { exact: true }),
+        []
+    );
 }
 
 export function useUpdateOrderStatus() {
-  return useMutation(
-    (values: { id: string; status: OrderStatus; comment: string }) => {
-      const { id, ...data } = values;
-      return axios.put(`${API_PATHS.order}/order/${id}/status`, data, {
-        headers: {
-          Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
-        },
-      });
-    }
-  );
+    return useMutation(
+        (values: { id: string; status: OrderStatus; comment: string }) => {
+            const { id, ...data } = values;
+            return axios.put(`${API_PATHS.order}/order/${id}/status`, data, {
+                headers: {
+                    Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+                },
+            });
+        }
+    );
 }
 
 export function useSubmitOrder() {
-  return useMutation((values: Omit<Order, "id">) => {
-    return axios.put<Omit<Order, "id">>(`${API_PATHS.order}/order`, values, {
-      headers: {
-        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
-      },
+    return useMutation((values: Omit<Order, "id">) => {
+        return axios.put<Omit<Order, "id">>(`${API_PATHS.order}/order`, values, {
+            headers: {
+                Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+            },
+        });
     });
-  });
 }
 
 export function useInvalidateOrder() {
-  const queryClient = useQueryClient();
-  return React.useCallback(
-    (id: string) =>
-      queryClient.invalidateQueries(["order", { id }], { exact: true }),
-    []
-  );
+    const queryClient = useQueryClient();
+    return React.useCallback(
+        (id: string) =>
+            queryClient.invalidateQueries(["order", { id }], { exact: true }),
+        []
+    );
 }
 
 export function useDeleteOrder() {
-  return useMutation((id: string) =>
-    axios.delete(`${API_PATHS.order}/order/${id}`, {
-      headers: {
-        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
-      },
-    })
-  );
+    return useMutation((id: string) =>
+        axios.delete(`${API_PATHS.order}/order/${id}`, {
+            headers: {
+                Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+            },
+        })
+    );
 }

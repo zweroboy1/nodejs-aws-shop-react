da63e4edc4b9f193d6083790078a051454ee4fc2
Oleksii Roman <fprognoz@gmail.com>
2026-06-22 02:09:34 +0300

    feat: update cart API endpoint to CloudFront HTTPS URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
     product: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
-    order: "http://zweroboy1-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
+    order: "https://d1pku5dugyuo9d.cloudfront.net/api/profile/cart",
     import: "https://39r1iqoj3f.execute-api.eu-central-1.amazonaws.com/prod",
     bff: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
-    cart: "http://zweroboy1-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
+    cart: "https://d1pku5dugyuo9d.cloudfront.net/api",
 };
 
 export default API_PATHS;

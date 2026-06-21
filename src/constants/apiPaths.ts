5ff1d42cc032726ae68e93d7d4713e1da04591e2
Oleksii Roman <fprognoz@gmail.com>
2026-06-22 01:57:00 +0300

    feat: update cart API endpoint to EBS deployment

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
     product: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
-    order: "https://pxf5ekte6d.execute-api.eu-central-1.amazonaws.com/prod/api/profile/cart",
+    order: "http://zweroboy1-cart-api-prod.eu-central-1.elasticbeanstalk.com/api/profile/cart",
     import: "https://39r1iqoj3f.execute-api.eu-central-1.amazonaws.com/prod",
     bff: "https://4yp23fppjg.execute-api.eu-central-1.amazonaws.com/prod",
-    cart: "https://pxf5ekte6d.execute-api.eu-central-1.amazonaws.com/prod/api",
+    cart: "http://zweroboy1-cart-api-prod.eu-central-1.elasticbeanstalk.com/api",
 };
 
 export default API_PATHS;
